594767c2604efd921dc5d4bbad06ab6076ee2d1c
Samuele Furnari <samuelefurnari9@gmail.com>
2024-03-16 14:12:03 +0100

    upgraded and tested MooveToken.sol

--- contracts/contracts/MooveToken.sol
+++ contracts/contracts/MooveToken.sol
@@ -1,9 +1,93 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/token/ERC721/ERC721.sol";
-import "@openzeppelin/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MooveToken is ERC721, Ownable {
-    constructor() ERC721("MooveToken", "MT") Ownable(msg.sender) {}
+    address s_owner;
+
+    struct Vehicle {
+        uint256 id;
+        string name;
+        string model;
+        uint256 priceVehicle;
+        bool available;
+        address owner;
+    }
+
+    mapping(uint256 => Vehicle) public detailsVehicle;
+    mapping(uint256 => bool) public availableVehicle;
+
+    uint256[] public allVehicle;
+
+    constructor(
+        string memory _nameToken,
+        string memory _symbolToken
+    ) ERC721(_nameToken, _symbolToken) Ownable(msg.sender) {
+        s_owner = msg.sender;
+    }
+
+    function addVehicle(
+        uint256 _id,
+        string memory _name,
+        string memory _model,
+        uint256 _priceVehicle
+    ) external onlyOwner {
+        detailsVehicle[_id] = Vehicle({
+            id: _id,
+            name: _name,
+            model: _model,
+            priceVehicle: _priceVehicle,
+            available: true,
+            owner: address(this)
+        });
+        availableVehicle[_id] = true;
+        allVehicle.push(_id);
+        _safeMint(address(this), _id);
+    }
+
+    function buyNFTVehicle(uint256 _idVehicle) external payable {
+        bool found = false;
+        uint256 indexVehicle;
+
+        for (uint i = 0; i < allVehicle.length; i++) {
+            if (allVehicle[i] == _idVehicle) {
+                found = true;
+                indexVehicle = i;
+                break;
+            }
+        }
+
+        uint256 priceVehicle = detailsVehicle[_idVehicle].priceVehicle;
+        address ownerVehicle = detailsVehicle[_idVehicle].owner;
+
+        require(found == true, "Vehicle doesn't found");
+        require(
+            msg.value >= priceVehicle,
+            "Doesn't have enough funds to buy this vehicle"
+        );
+
+        transferFrom(ownerVehicle, msg.sender, _idVehicle);
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public override {
+        super.transferFrom(_from, _to, _tokenId);
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes memory
+    ) external pure returns (bytes4) {
+        return
+            bytes4(
+                keccak256("onERC721Received(address,address,uint256,bytes)")
+            );
+    }
 }
--- contracts/test/MooveToken.ts
+++ contracts/test/MooveToken.ts
@@ -3,5 +3,60 @@ import { ethers } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("MooveToken", () => {
-  async function deploy() {}
+  async function deploy() {
+    const tokenName = "MooveToken";
+    const tokenSymbol = "MT";
+    const [owner, otherAccount] = await ethers.getSigners();
+    const MooveToken = await ethers.deployContract("MooveToken", [
+      tokenName,
+      tokenSymbol,
+    ]);
+
+    return { tokenName, tokenSymbol, owner, otherAccount, MooveToken };
+  }
+  describe("Testing general info about ERC721 token", () => {
+    it("Should return the correct token name and symbol", async () => {
+      const { tokenName, tokenSymbol, MooveToken } = await loadFixture(deploy);
+
+      expect(await MooveToken.name()).to.equal(tokenName);
+      expect(await MooveToken.symbol()).to.equal(tokenSymbol);
+    });
+  });
+  describe("Testing addVehicle function", () => {
+    it("Should revert if the sender is not the Owner", async () => {
+      const { otherAccount, MooveToken } = await loadFixture(deploy);
+
+      await expect(
+        MooveToken.connect(otherAccount).addVehicle(
+          12345,
+          "Bike",
+          "Electric",
+          ethers.parseEther("0.1")
+        )
+      ).to.be.reverted;
+    });
+    it("Should add the vehicle and generete the ERC721 token", async () => {
+      const { MooveToken } = await loadFixture(deploy);
+
+      await MooveToken.addVehicle(
+        12345,
+        "Bike",
+        "Electric",
+        ethers.parseEther("0.1")
+      );
+
+      expect(await MooveToken.availableVehicle(12345)).to.equal(true);
+      expect(await MooveToken.detailsVehicle(12345)).to.deep.equal([
+        BigInt(12345),
+        "Bike",
+        "Electric",
+        ethers.parseEther("0.1"),
+        true,
+        await MooveToken.getAddress(),
+      ]);
+      expect(
+        await MooveToken.balanceOf(await MooveToken.getAddress())
+      ).to.equal(1);
+    });
+  });
 });
