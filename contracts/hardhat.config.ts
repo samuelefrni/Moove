6db1a3d787187ebf5038fdaa6cea205c9a79dcec
Samuele Furnari <samuelefurnari9@gmail.com>
2024-05-10 10:09:05 +0200

    deployment

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,10 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { ETHERSCAN_API, INFURA_KEY, SEPOLIA_PK } from "./cred";
+
+//
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   paths: {
     artifacts: "../frontend/src/artifacts",
+  },
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_KEY}`,
+      accounts: [SEPOLIA_PK],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API,
   },
 };
 
