05e0a4d2bed2033d0ead641136605935ce1a5b2f
Samuele Furnari <samuelefurnari9@gmail.com>
2024-05-13 10:05:26 +0200

    instruction hardhat.config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,7 +2,12 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import { ETHERSCAN_API, INFURA_KEY, SEPOLIA_PK } from "./cred";
 
-//
+//  The information you can see in the hardhat.config file will not be correct for the
+//  compiler on your local enviroment. This is because the "cred" file was only present
+//  in the distribution. This sensitive file will not be present in the GitHub repository
+//  so before running any command:
+//  Be sure to comment the import from "./cred" (line 3)
+//  Be sure to comment from the "networks" (line 17) to "etherscan" (line 25) properties
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
